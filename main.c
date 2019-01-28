fbb128fd55144778f9be9f86188f35040755b25b
Prgy24 <39917177+Prgy24@users.noreply.github.com>
2019-01-28 15:43:16 +0530

    create _5 task

--- main.c
+++ main.c
@@ -5,20 +5,41 @@
 
 OS_STK task_stk1[STACK_SIZE];
 OS_STK task_stk2[STACK_SIZE];
+OS_STK task_stk3[STACK_SIZE];
+OS_STK task_stk4[STACK_SIZE];
+OS_STK task_stk5[STACK_SIZE];
 
 void task1(void *arg);
 void task2(void *arg);
+void task3(void *arg);
+void task4(void *arg);
+void task5(void *arg);
+
+
 int main(void)
 {
 	INT8U ret;
 	OSInit();
-	ret = OSTaskCreate(&task1, (void*)0, &task_stk1[STACK_SIZE - 1], 0);
+
+	ret = OSTaskCreate(&task1, (void*)0, &task_stk1[STACK_SIZE - 1], 1);
 	if(ret != OS_ERR_NONE)
 		printf("Task1 creation failed..\n\r");
-	//OSStart();
-	ret = OSTaskCreate(&task2, (void*)0, &task_stk2[STACK_SIZE - 1], 1);
-		if(ret != OS_ERR_NONE)
-			printf("Task2 creation failed..\n\r");
+
+	ret = OSTaskCreate(&task2, (void*)0, &task_stk2[STACK_SIZE - 1], 2);
+	if(ret != OS_ERR_NONE)
+		printf("Task2 creation failed..\n\r");
+
+	ret = OSTaskCreate(&task3, (void*)0, &task_stk3[STACK_SIZE - 1], 3);
+	if(ret != OS_ERR_NONE)
+		printf("Task1 creation failed..\n\r");
+
+	ret = OSTaskCreate(&task4, (void*)0, &task_stk4[STACK_SIZE - 1], 4);
+	if(ret != OS_ERR_NONE)
+		printf("Task2 creation failed..\n\r");
+
+	ret = OSTaskCreate(&task5, (void*)0, &task_stk5[STACK_SIZE - 1], 5);
+	if(ret != OS_ERR_NONE)
+		printf("Task2 creation failed..\n\r");
 
 	OSStart();
 
@@ -34,7 +55,7 @@ void task1(void *arg)
 	for( ; ; )
 	{
 		printf("In Task1..\n\r");
-		OSTimeDly(1*OS_TICKS_PER_SEC);
+		OSTimeDly(0.2*OS_TICKS_PER_SEC);
 	}
 }
 void task2(void *arg)
@@ -43,6 +64,35 @@ void task2(void *arg)
 	for( ; ; )
 	{
 		printf("In Task2..\n\r");
+		OSTimeDly(0.4*OS_TICKS_PER_SEC);
+	}
+}
+
+void task3(void *arg)
+{
+	OS_CPU_SysTickInit(SystemCoreClock/OS_TICKS_PER_SEC);
+	for( ; ; )
+	{
+		printf("In Task3..\n\r");
+		OSTimeDly(0.6*OS_TICKS_PER_SEC);
+	}
+}
+void task4(void *arg)
+{
+	OS_CPU_SysTickInit(SystemCoreClock/OS_TICKS_PER_SEC);
+	for( ; ; )
+	{
+		printf("In Task4..\n\r");
+		OSTimeDly(0.8*OS_TICKS_PER_SEC);
+	}
+}
+
+void task5(void *arg)
+{
+	OS_CPU_SysTickInit(SystemCoreClock/OS_TICKS_PER_SEC);
+	for( ; ; )
+	{
+		printf("In Task5..\n\r");
 		OSTimeDly(1*OS_TICKS_PER_SEC);
 	}
 }
