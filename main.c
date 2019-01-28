9f6bbc6b2f814f411f4d503f6a4413f2747b4ba2
Prgy24 <39917177+Prgy24@users.noreply.github.com>
2019-01-28 15:46:35 +0530

    create 2 task quits

--- main.c
+++ main.c
@@ -1,16 +1,13 @@
+
 #include"includes.h"
 #include"system_stm32f10x.h"
 
 OS_STK task_Stk1[100];
 OS_STK task_Stk2[100];
-OS_STK task_Stk3[100];
-OS_STK task_Stk4[100];
 
 
 void task1(void *arg);
 void task2(void *arg);
-void task3(void *arg);
-void task4(void *arg);
 
 INT8U ret;
 
@@ -21,30 +18,29 @@ int main(void)
 	if(ret != OS_ERR_NONE)
 		printf("task1 creation failed\n\r");
 
+	ret = OSTaskCreate(&task2,(void *)0,&task_Stk2[100-1],2);
+	if(ret != OS_ERR_NONE)
+		printf("task2 creation failed\n\r");
+
 	OSStart();
 
     while(1)
     {
+    	printf("hello\n\r");
     }
 }
 
 void task1(void* ar)
 {
+	INT8U count=0;
+
 	OS_CPU_SysTickInit(SystemCoreClock/OS_TICKS_PER_SEC);
-	ret = OSTaskCreate(&task2,(void *)0,&task_Stk2[100-1],2);
-	if(ret != OS_ERR_NONE)
-		printf("task2 creation failed\n\r");
-
-	ret = OSTaskCreate(&task3,(void *)0,&task_Stk3[100-1],3);
-	if(ret != OS_ERR_NONE)
-		printf("task3 creation failed\n\r");
-
-	ret = OSTaskCreate(&task4,(void *)0,&task_Stk4[100-1],4);
-	if(ret != OS_ERR_NONE)
-		printf("task4 creation failed\n\r");
 
 	for(;;)
 	{
+		count++;
+		if(count > 3)
+		   OSTaskDel(OS_PRIO_SELF);
 		printf("IN TASK1\n\r");
 		OSTimeDly(1*OS_TICKS_PER_SEC);
 	}
@@ -52,35 +48,21 @@ void task1(void* ar)
 
 void task2(void* ar)
 {
-	INT8U cond=0;
+	INT8U count=0;
 
 	OS_CPU_SysTickInit(SystemCoreClock/OS_TICKS_PER_SEC);
 	for(;;)
 	{
-		printf("IN TASK2\n\r");
-		OSTaskDel(OS_PRIO_SELF);
+		count++;
+		if(count > 3)
+		    OSTaskDel(OS_PRIO_SELF);
+		 printf("IN TASK2\n\r");
+		 OSTimeDly(1*OS_TICKS_PER_SEC);
+
 	}
 }
 
-void task3(void* ar)
-{
-	OS_CPU_SysTickInit(SystemCoreClock/OS_TICKS_PER_SEC);
-	for(;;)
-	{
-		printf("IN TASK3\n\r");
-		OSTaskDel(OS_PRIO_SELF);
-	}
-}
 
-void task4(void* ar)
-{
-	OS_CPU_SysTickInit(SystemCoreClock/OS_TICKS_PER_SEC);
-	for(;;)
-	{
-		printf("IN TASK4\n\r");
-		OSTaskDel(OS_PRIO_SELF);
-	}
-}
 
 
 
