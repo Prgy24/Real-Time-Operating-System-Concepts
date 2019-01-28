3933c23f7a49fa4bc3d3418c6c3ed9f5e65c1e50
Prgy24 <39917177+Prgy24@users.noreply.github.com>
2019-01-28 15:48:35 +0530

    signalling semaphore

--- main.c
+++ main.c
@@ -1,82 +1,83 @@
+
 #include"includes.h"
 #include"system_stm32f10x.h"
-#include"OS_cpu.h"
 
-void pro1(void *arg);
-void pro2(void *arg);
-void con(void *arg);
 
 #define STACK_SIZE 100
-OS_STK task_stk1[100];
-OS_STK task_stk2[100];
-OS_STK task_stk3[100];
+OS_EVENT *sem1;
+INT8U i, ret, count=0, mystring[20];
 
-INT8U a=0, i;
+OS_STK write_Stk1[STACK_SIZE];
+OS_STK write_Stk2[STACK_SIZE];
+
+void write1(void *arg);
+void write2(void *arg);
+
 
 int main(void)
 {
-	INT8U ret;
 	OSInit();
-	ret=OSTaskCreate(&pro1,(void *)0,&task_stk1[100-1],1);
-	if(ret!=OS_ERR_NONE)
-	{
-		printf("task(producer 1) creation failed\n\r");
-	}
-	ret=OSTaskCreate(&pro2,(void *)0,&task_stk2[100-1],2);
-	if(ret!=OS_ERR_NONE)
-	{
-		printf("task(producer 2) creation failed\n\r");
-	}
-	ret=OSTaskCreate(&con,(void *)0,&task_stk3[100-1],0);
-	if(ret!=OS_ERR_NONE)
-	{
-		printf("task(consumer) creation failed\n\r");
-	}
+	ret = OSTaskCreate(&write1,(void *)0,&write_Stk1[STACK_SIZE - 1], 1);
+	if(ret != OS_ERR_NONE)
+		printf("task1 creation failed\n\r");
+
+	ret = OSTaskCreate(&write2,(void *)0,&write_Stk2[STACK_SIZE - 1], 2);
+	if(ret != OS_ERR_NONE)
+		printf("task2 creation failed\n\r");
+
+	sem1 = OSSemCreate(1);
 
 	OSStart();
+
     while(1)
     {
-    	printf("hello..\n\r");
+    	printf("hello\n\r");
     }
 }
 
-
-void pro1(void *arg)
+void write1(void* ar)
 {
-	OS_CPU_SR cpu_sr;
-	for( ; ; )
-	{
-		printf("producer 1\n\r");
-		OS_ENTER_CRITICAL();
-		a = a+10;
-		OS_EXIT_CRITICAL();
-		OSTimeDly(5*OS_TICKS_PER_SEC);
-	}
+	INT8U j=0, err;
 
-}
-
-void pro2(void *arg)
-{
-	OS_CPU_SR cpu_sr;
-	for( ; ; )
-	{
-		printf("producer 2\n\r");
-		OS_ENTER_CRITICAL();
-		a = a+5;
-		OS_EXIT_CRITICAL();
-		OSTimeDly(0.7*OS_TICKS_PER_SEC);
-	}
-
-}
-
-void con(void *arg)
-{
 	OS_CPU_SysTickInit(SystemCoreClock/OS_TICKS_PER_SEC);
-	for( ; ; )
-	{
-		printf("consumer\n\r");
-		printf("%d\n\r",a);
-		OSTimeDly(2*OS_TICKS_PER_SEC);
-	}
 
+	for(;;)
+	{
+		OSSemPend(&sem1, 100 , &err);
+		mystring[i] = 'a' + j;
+		i++;
+		j++;
+		count++;
+		if(count > 20)
+			OSTaskDel(OS_PRIO_SELF);
+		printf("IN write1 %s\n\r", mystring);
+		OSTimeDly(1*OS_TICKS_PER_SEC);
+	}
 }
+
+void write2(void* ar)
+{
+	INT8U j=0, res;
+
+	for(;;)
+	{
+		res = OSSemPost(&sem1);
+		mystring[i] = '1' + j;
+		i++;
+		j++;
+		count++;
+		if(count > 19)
+			OSTaskDel(OS_PRIO_SELF);
+
+		printf("IN write2 %s\n\r", mystring);
+
+		 OSTimeDly(1*OS_TICKS_PER_SEC);
+
+	}
+}
+
+
+
+
+
+
