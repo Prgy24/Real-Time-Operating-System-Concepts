caf95b8be71536fb3b1f18bc1162079f8e5a7479
Prgy24 <39917177+Prgy24@users.noreply.github.com>
2019-01-28 15:47:23 +0530

    master_slave

--- main.c
+++ main.c
@@ -2,68 +2,85 @@
 #include"includes.h"
 #include"system_stm32f10x.h"
 
-OS_STK task_Stk1[100];
-OS_STK task_Stk2[100];
+OS_STK task_master[100];
+OS_STK task_slave1[100];
+OS_STK task_slave2[100];
 
 
-void task1(void *arg);
-void task2(void *arg);
+
+void master(void *arg);
+void slave1(void *arg);
+void slave2(void *arg);
+
 
 INT8U ret;
 
 int main(void)
 {
 	OSInit();
-	ret = OSTaskCreate(&task1,(void *)0,&task_Stk1[100-1],1);
-	if(ret != OS_ERR_NONE)
-		printf("task1 creation failed\n\r");
 
-	ret = OSTaskCreate(&task2,(void *)0,&task_Stk2[100-1],2);
+	ret = OSTaskCreate(&slave1,(void *)0,&task_slave1[100-1], 0);
 	if(ret != OS_ERR_NONE)
-		printf("task2 creation failed\n\r");
+		printf("slave1 creation failed\n\r");
+
+	ret = OSTaskCreate(&slave2,(void *)0,&task_slave2[100-1], 1);
+	if(ret != OS_ERR_NONE)
+		printf("slave2 creation failed\n\r");
+
+	ret = OSTaskCreate(&master,(void *)0,&task_master[100-1], 2);
+		if(ret != OS_ERR_NONE)
+			printf("master creation failed\n\r");
 
 	OSStart();
 
     while(1)
     {
-    	printf("hello\n\r");
+    	printf("Hi...\n\r");
     }
 }
 
-void task1(void* ar)
+
+void slave1(void* ar)
 {
-	INT8U count=0;
+	//OS_CPU_SysTickInit(SystemCoreClock/OS_TICKS_PER_SEC);
+
+	for(;;)
+	{
+		printf("IN slave1\n\r");
+		//OSTimeDly(1*OS_TICKS_PER_SEC);
+		OSTaskSuspend(0);
+	}
+}
+
+
+void slave2(void* ar)
+{
+	//OS_CPU_SysTickInit(SystemCoreClock/OS_TICKS_PER_SEC);
+
+	for(;;)
+	{
+		printf("IN slave2\n\r");
+		//OSTimeDly(1*OS_TICKS_PER_SEC);
+		OSTaskSuspend(1);
+	}
+}
+
+
+void master(void* ar)
+{
 
 	OS_CPU_SysTickInit(SystemCoreClock/OS_TICKS_PER_SEC);
 
 	for(;;)
 	{
-		count++;
-		if(count > 3)
-		   OSTaskDel(OS_PRIO_SELF);
-		printf("IN TASK1\n\r");
-		OSTimeDly(1*OS_TICKS_PER_SEC);
+		printf("IN master\n\r");
+
+		 OSTimeDly(0.5*OS_TICKS_PER_SEC);
+		 OSTaskResume(0);
+		 OSTimeDly(0.7*OS_TICKS_PER_SEC);
+		 OSTaskResume(1);
 	}
 }
-
-void task2(void* ar)
-{
-	INT8U count=0;
-
-	OS_CPU_SysTickInit(SystemCoreClock/OS_TICKS_PER_SEC);
-	for(;;)
-	{
-		count++;
-		if(count > 3)
-		    OSTaskDel(OS_PRIO_SELF);
-		 printf("IN TASK2\n\r");
-		 OSTimeDly(1*OS_TICKS_PER_SEC);
-
-	}
-}
-
-
-
 
 
 
